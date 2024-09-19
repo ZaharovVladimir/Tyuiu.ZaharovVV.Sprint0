632feec7f730efa55ae1ff5f9237c2c678ea7667
NightmarDark <chibisntb@yandex.ru>
2024-09-20 00:06:12 +0500

    task2.1

--- Tyuiu.ZaharovVV.Sprint0.Task1.V0.Lib/DataService.cs
+++ Tyuiu.ZaharovVV.Sprint0.Task1.V0.Lib/DataService.cs
@@ -1,7 +1,16 @@
-﻿namespace Tyuiu.ZaharovVV.Sprint0.Task1.V0.Lib
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Tyuiu.ZaharovVV.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}";
+        }
     }
 }
--- Tyuiu.ZaharovVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
+++ Tyuiu.ZaharovVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -1,11 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZaharovVV.Sprint0.Task2.V0.Lib;
+
 namespace Tyuiu.ZaharovVV.Sprint0.Task2.V0.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckGetMessageValid()
         {
+            var name = "Владимир";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Владимир", res);
         }
     }
 }
