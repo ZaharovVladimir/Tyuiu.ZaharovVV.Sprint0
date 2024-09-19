2ee18f9f3cfaaf02c978daff5f41577e76234255
NightmarDark <chibisntb@yandex.ru>
2024-09-20 01:57:36 +0500

    Task4

---
+++ Tyuiu.ZaharovVV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿namespace Tyuiu.ZaharovVV.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multipication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
