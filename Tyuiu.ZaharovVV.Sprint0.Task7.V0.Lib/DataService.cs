a1d111060469d28cfd409be2ce7c9d029ddc9169
NightmarDark <chibisntb@yandex.ru>
2024-09-20 05:36:29 +0500

    Task7

--- Tyuiu.ZaharovVV.Sprint0.Task7.V0.Lib/DataService.cs
+++ Tyuiu.ZaharovVV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -2,6 +2,14 @@
 {
     public class DataService
     {
-
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
     }
 }
--- Tyuiu.ZaharovVV.Sprint0.Task7.V0.Test/DataServiceTest.cs
+++ Tyuiu.ZaharovVV.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -8,8 +8,14 @@ namespace Tyuiu.ZaharovVV.Sprint0.Task7.V0.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void Check()
+        public void CheckAdditionArraysValid()
         {
+            int[] nums1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums3 = new int[] { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
 
         }
     }
