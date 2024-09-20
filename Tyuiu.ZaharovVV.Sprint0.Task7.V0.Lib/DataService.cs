021fc77e80a691b8f06c295c5727040ecccbefd6
NightmarDark <chibisntb@yandex.ru>
2024-09-20 05:10:55 +0500

    task5

---
+++ Tyuiu.ZaharovVV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.ZaharovVV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.ZaharovVV.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaharovVV.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.ZaharovVV.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Check()
+        {
+
+        }
+    }
+}
